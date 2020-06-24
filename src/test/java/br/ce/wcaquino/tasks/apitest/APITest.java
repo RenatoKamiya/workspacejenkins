6812b919044133320284a9087abe7b2e6c356353
RenatoKamiya <RenatoKamiya@hotmail.com>
2020-06-23 22:23:26 -0300

    first commit

---
+++ src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
@@ -0,0 +1,43 @@
+package br.ce.wcaquino.tasks.apitest;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class APITest {
+	
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = "http://34.95.219.233:8001/tasks-backend";
+	}
+	
+	@Test
+	public void deveRetornarTarefas() {
+		RestAssured.given()
+		.when()
+			.get("/todo")
+		.then()
+			.statusCode(200)
+			;
+	}
+	
+	@Test
+	public void naoDeveAdicionarTarefaInvalida() {
+		RestAssured.given()
+			.body("{\"task\":\"Teste via API\",\"dueDate\": \"2010-12-30\"}")
+			.contentType(ContentType.JSON)
+		
+		.when()
+			.post("/todo")
+		.then()
+			.statusCode(400)
+			.log().all()
+			.body("message", CoreMatchers.is("Due date must not be in past"))
+		;
+	}
+
+}
+
